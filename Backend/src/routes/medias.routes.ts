abeb008b1d9c848b0dc9eb0d408bd6d47bb70bd2
liolle <laurentlionel20@hotmail.com>
2023-08-22 17:49:56 +0200

    [Update] - claim images

--- Backend/src/routes/medias.routes.ts
+++ Backend/src/routes/medias.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { getMedia, getUserMedia, upload } from '../controllers/media';
+import { claim, getMedia, getUserMedia, upload } from '../controllers/media';
 import verifyJwt from '../middlewares/auth';
 
 
 router.get('/',verifyJwt,getMedia)
 router.get('/all',verifyJwt,getUserMedia)
 router.post('/upload',verifyJwt,upload)
+router.post('/claim',verifyJwt,claim)
 
 module.exports = router
