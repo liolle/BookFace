9a76333a8f2da41b96134d55bf910de400645a59
liolle <laurentlionel20@hotmail.com>
2023-09-17 14:42:21 +0200

    ..

--- Frontend/src/Pages/BookMarks.tsx
+++ Frontend/src/Pages/BookMarks.tsx
@@ -9,7 +9,7 @@ const BookMarks: React.FC = () => {
 
     React.useEffect(() => {
       const handleResize = () => {
-        setIsMobile(window.innerWidth <= 768); // Modifier cette valeur en fonction de votre définition d'un écran de téléphone
+        setIsMobile(window.innerWidth <= 768); 
       };
   
       window.addEventListener('resize', handleResize);
